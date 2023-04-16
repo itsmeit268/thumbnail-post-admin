9bc7e92cef373d78d6c07bb6f695a829920f389e
itsmeit <buivanloi.2010@gmail.com>
2023-04-16 12:48:58 +0700

    Change logic, fix mobile, add filter

--- thumbnail-post-admin.php
+++ thumbnail-post-admin.php
@@ -14,30 +14,64 @@ Copyright 2023 itsmeit.co (email: buivanloi.2010@gmail.com)
 
 class thumbnailPostAdmin
 {
-    function __construct() {
-        add_action('init', array($this, 'thumbnailPostAdmin'), 99);
+    /**
+     * @var array
+     */
+    private $excluded_posttypes = [];
+
+    /**
+     * thumbnailPostAdmin constructor.
+     */
+    public function __construct()
+    {
+        add_filter('manage_posts_columns', array($this, 'add_thumbnail_column'), 10, 1);
+        add_action('manage_posts_custom_column', array($this, 'manage_posts_custom_column'), 10, 2);
     }
 
-    public function thumbnailPostAdmin()
+    /**
+     * @param $columns
+     * @return array|mixed
+     */
+    public function add_thumbnail_column($columns)
     {
-        add_filter('manage_posts_columns', 'add_thumbnail_column', 10, 1);
-        function add_thumbnail_column($columns)
-        {
-            $new_columns = array();
-            $new_columns['thumbnail'] = __('Ảnh đại diện', 'text-domain');
+        $new_columns = array();
+        $new_columns['thumbnail'] = __('Thumbnail', 'text-domain');
+        if (!wp_is_mobile() && !in_array($this->getCurrentAdminPostType(), $this->getExcludedPostTypes())) {
             $columns = array_merge($new_columns, $columns);
-            return $columns;
         }
+        return $columns;
+    }
 
-        add_action('manage_posts_custom_column', 'display_thumbnail_column', 10, 2);
-        function display_thumbnail_column($column, $post_id)
-        {
-            if ($column === 'thumbnail') {
-                echo get_the_post_thumbnail($post_id, array(120, 9999));
-            }
+
+    /**
+     * @param $column
+     * @param $post_id
+     */
+    public function manage_posts_custom_column($column, $post_id)
+    {
+        if ($column === 'thumbnail' && has_post_thumbnail( $post_id ) && !in_array($this->getCurrentAdminPostType(), $this->getExcludedPostTypes()))
+            echo get_the_post_thumbnail($post_id, array(120, 9999));
+    }
+
+    /**
+     * @return null
+     */
+    private function getCurrentAdminPostType() {
+        global $post, $typenow, $current_screen;
+        return $post && $post->post_type ? $post->post_type : ($typenow ? $typenow : ($current_screen && $current_screen->post_type ? $current_screen->post_type : (isset($_REQUEST['post_type']) ? sanitize_key($_REQUEST['post_type']) : null)));
+    }
+
+
+    /**
+     * @return array
+     */
+    private function getExcludedPostTypes()
+    {
+        if (empty($this->excluded_posttypes)) {
+            $this->excluded_posttypes = (array) apply_filters('thumbnail/exclude_posttype', $this->excluded_posttypes);
         }
+        return $this->excluded_posttypes;
     }
 }
 
 new thumbnailPostAdmin();
-
